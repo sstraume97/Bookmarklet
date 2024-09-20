9907fc44222055f1e0e3aa5284c7b23e32be5b2f
Sondre Bogen-Straume <sstraume97@outlook.com>
2024-09-20 17:03:59 +0200

    Nye alternativer lagt til i Master Arkiv.js
    Nå mulig å: 
    Arkivere sider på
     1 = Archive.today
     2 = Web Archive
     3 = PreserveTube
    Og finne arkiverte sider på
     4 = Archive.today
     5 = Web Archive
     6 = Google Cache

--- Arkiv/Master Arkiv.js
+++ Arkiv/Master Arkiv.js
@@ -1,15 +1,34 @@
 /*
-Marker teksten fra linje 5 til 16, dra deretter teksten til bokmerkeraden.
+Marker teksten fra linje 5 til 31, dra deretter teksten til bokmerkeraden.
+
+Teksten "Hvilke bokmerke ønsker du å aktivere? \n"
+Var opprinnelig plassert her: javascript:var a = prompt(Hvilke bokmerke ønsker du å aktivere? \n"---- Arkiver side ---- \n   1 = Archive.today \n   2 = Web Archive \n   3 = PreserveTube \n---- Finn arkivert side ---- \n   4 = Archive.today \n   5 = Web Archive \n   6 = Google Cache");
 */
 
-javascript:var a = prompt("Hvilke bokmerke ønsker du å aktivere? \n1 = Archive.today \n2 = PreserveTube");
+javascript:var a = prompt("---- Arkiver side ---- \n   1 = Archive.today \n   2 = Web Archive \n   3 = PreserveTube \n---- Finn arkivert side ---- \n   4 = Archive.today \n   5 = Web Archive \n   6 = Google Cache");
 (function() {
      if (a == 1) {
          var url = encodeURIComponent(window.location.href); window.open('http://archive.today/?run=1&url='+encodeURIComponent(document.location));
       } 
          else if (a == 2) {
             var url = encodeURIComponent(window.location.href);
+            window.open('https://web.archive.org/save/' + url, '_blank');
+      }
+        else if (a == 3) {
+            var url = encodeURIComponent(window.location.href);
             window.open('https://preservetube.com/save?url=' + url, '_blank');
-      } 
+      }
+        else if (a == 4) {
+            var url = encodeURIComponent(window.location.href);
+            window.open('https://archive.today/latest/'+location.href);
+      }
+       else if (a == 5) {
+            var url = encodeURIComponent(window.location.href);
+            window.open('https://web.archive.org/web/2/'+location.href);
+      }
+       else if (a == 6) {
+            var url = encodeURIComponent(window.location.href);
+            window.open('https://webcache.googleusercontent.com/search?q=cache:'+location.href);
+      }
 }
 )();
