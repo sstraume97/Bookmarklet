b8ff3f2db62a15c3775acc646b0ac5da67c9cfd5
Sondre Bogen-Straume <sstraume97@outlook.com>
2024-09-20 20:14:56 +0200

    Create Fjern CSS.js

---
+++ Ny/Fjern CSS.js
@@ -0,0 +1,3 @@
+// Fjerner CSS (formatering) fra gjeldene side
+
+javascript:%20(function()%20%7B%20var%20i,%20x;%20for%20(i%20=%200;%20x%20=%20document.styleSheets%5Bi%5D;%20++i)%20x.disabled%20=%20true;%20%7D)();
